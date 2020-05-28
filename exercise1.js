de3f477136040ca0210bdc99edc7a24ce63c2bcc
Gowtham Katta <gowthamkatta@gmail.com>
2020-05-28 03:13:24 -0500

    Saving progess on Exercise 1 on JS

--- exercise1.js
+++ exercise1.js
@@ -7,10 +7,29 @@
 
 //your code...
 
+//Part 1
 
+var Temperature = 100;
+var Fahrenheit;
+var Celsius;
 
+function F_to_C() 
+{
+    Celsius = (Temperature - 32) * (5 / 9)
+    console.log(' Celsius = ' + Celsius);
+}
 
+F_to_C();
 
+//Part 2
+
+function C_to_F()
+{
+    Fahrenheit = (Celsius * (9/5)) + 32
+    console.log(' Fahrenheit = ' + Fahrenheit);
+}
+
+C_to_F();
 
 /************************************************************* */
 // Problem 2:
@@ -18,11 +37,15 @@
 
 //your code...
 
+var myAge = 22;
 
+function To_Vote() 
+{
+    myAge >= 18 ? 
+        console.log(' Yes, you are able to vote ') : console.log(' No, you are NOT able to vote ');
+}
 
-
-
-
+To_Vote(myAge);
 
 /************************************************************* */
 // Problem 3:
@@ -30,11 +53,17 @@
 
 //your code...
 
+var String = "The five boxing wizards jump quickly.";
 
+function Split_String(String)
+{
+    var Array = String.split(' ');
+    console.log(Array);
+    var oldString = Array.join(' ');
+    console.log(oldString);
+}
 
-
-
-
+Split_String(String);
 
 /************************************************************* */
 // Problem 4:
@@ -42,9 +71,16 @@
 
 //your code...
 
+var MyNumber = 9721234567;
 
+function Reverse_Number(MyNumber) 
+{
+    var Number_String = MyNumber.toString();
+    var Number_Array = Number_String.split('');
+    console.log(Number_Array.reverse().join(''));
+}
 
-
+Reverse_Number(MyNumber);
 
 /************************************************************* */
 // Problem 5:
@@ -64,8 +100,16 @@
 
 //your code...
 
+function Odd_Or_Even()
+{
+    for (var i = 0; i <= 15; i++)
+    {
+        if (i % 2 == 0) console.log(i + ' ' + "= even");
+        else console.log(i + ' ' + "= odd");
+    }
+}
 
-
+Odd_Or_Even()
 
 /************************************************************* */
 // Problem 7:
@@ -73,6 +117,17 @@
 
 //your code...
 
+function TEKcamp()
+{
+    for (var i = 1; i <= 100; i++)
+    {
+        if((i % 3 == 0) && (i % 5 == 0)) console.log('TEKcamp');
+        else if(i % 3 == 0) console.log('TEK');
+        else if(i % 5 == 0) console.log('camp');
+    }
+}
+
+TEKcamp()
 
 /************************************************************* */
 // Problem 8:
@@ -83,6 +138,15 @@ const nums = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
 
 //your code...
 
+function NumsArray()
+{
+    for(var i = 0; i < nums.length; i++)
+    {
+        if (nums[i] % 3 == 0) console.log(nums[i])
+    }
+}
+
+NumsArray()
 
 
 // Problem 9:
@@ -94,9 +158,24 @@ const adjectiveArray = [ 'salty', 'spicy', 'sour', 'sweet', 'rich','creamy','ama
 // "for" loop that console.log()'s a sentence for each
 // corresponding value in the arrays. Add the word "is" or "are" depending on if the food is singular or plural.  "Potatoes are salty", "Lemon is sour".
 
+function Hungry()
+{
+    var school = foodArray.pop();
+    foodArray.push(school.school); 
+    console.log(school.school);
+    
+    for(var i = 0; i < foodArray.length; i++)
+    {
+        if(i == 2 ^ i == 4 ^ i == 5 ^ i == 6) console.log(foodArray[i] + ' is ' + adjectiveArray[i]);
+        else console.log(foodArray[i] + ' are ' + adjectiveArray[i]);
+    }
+}
+
+Hungry()
+
 /************************************************************* */
 // Bonus Assignment: Create 4 mathematical function expressions, add,subtract,multiply,divide.  put them in an array, and create a doMath() function that randomly does one of the 4 operations whenever it is run.  the doMath() function should print out what mathetmatical function was carried out.  The doMath() function should return the computed value of any operation performed.
-const operations = [];
+const operations = ['7 + 8', '24 - 6', '8 * 3', '42 / 7'];
 function doMath() {};
 
 

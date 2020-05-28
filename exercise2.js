cd44f7727f494525fba854ee526ca3ddae087a92
Gowtham Katta <gowthamkatta@gmail.com>
2020-05-28 05:17:57 -0500

    Saving progress on Exercise 2 on JS

--- exercise2.js
+++ exercise2.js
@@ -8,11 +8,19 @@ for(let i=0; i<10; i++) {
     console.log(" the value of i in the loop is : " + i);
 }
 
-
 //your code...
 
+function Refactor()
+{
+    let i = 0;
+    while (i < 10)
+    {
+    console.log(' the value of i in the loop is : ' + i);
+    i++;
+    }
+}
 
-
+Refactor();
 
 /************************************************************* */
 // Problem 2:
@@ -21,6 +29,19 @@ for(let i=0; i<10; i++) {
 
 //your code...
 
+function add(){
+    return 2 + 30
+}
+
+function multiply(product){
+    return product * 20
+}
+
+function divide(product){
+    return product / Math.pow(10, 2)
+}
+
+console.log(divide(multiply(add())))
 
 /************************************************************* */
 //Problem 3:
@@ -39,13 +60,11 @@ for(let i=0; i<10; i++) {
 // undefined
 // ""
 
-
-
-
 /************************************************************* */
 // Problem 4:
 // Refactor the following code using a switch statement:
 
+/*
 const day = "friday";
 
 if(day === "monday") {
@@ -61,11 +80,46 @@ if(day === "monday") {
 } else {
     console.log("It's a weekend!")
 }
+*/
 
+const day= "friday";
 
+function Switch()
+{
+    switch (day)
+    {
+        case "monday":
+            console.log("we got a long week ahead of us...");
+            break;
+
+        case "tuesday":
+            console.log("tuesday's are still better than mondays, but LONG way to go still");
+            break;
+
+        case "wednesday":
+            console.log("We are smack dab in the middle of the week");
+            break;
+
+        case "thursday":
+            console.log("Thursday night... the mood is right");
+            break;
+
+        case "friday":
+            console.log("TGIF.  Friday truly is the best day of the week!")
+            break;
+
+        default:
+            console.log("It's a weekend!")
+            break;
+    }
+}
+
+Switch();
 
 /************************************************************* */
 // Problem 5: Refactor the following functions to use a ternary expression:
+
+/*
 const age = 10;
 if (age > 21) console.log("adult"); else {
     console.log("minor");
@@ -78,7 +132,18 @@ if (age > 13 && age < 19) console.log('teen'); else {
 if (age > 65) console.log("retired"); else {
     console.log("still working...");
 }
+*/
 
+const age = 10
+
+function Ternary()
+{
+    age > 21 ? console.log(' adult ') : console.log(' minor ');
+    age > 13 && age < 19 ? console.log(' teen ') : console.log(' not a teenager ');
+    age > 65 ? console.log(' retired ') : console.log('still working...');
+}
+
+Ternary();
 
 /************************************************************* */
 // Problem 6: Create an object literal that represents yourself.  set it to a variable that appropriately describes the object.  Include the following properties:
@@ -97,20 +162,73 @@ if (age > 65) console.log("retired"); else {
 
 //your code...
 
+function meExpanded()
+{
+    var me =
+    {
+        name: "Gowtham Katta", 
+        age: "22", 
+        gender: "Male", 
+        hobbies: ['Running', 'Hiking', 'Netflix'],
+        profession: "Student",
+        education: "Bachelor of Science and Arts in Biology",
+        learn: function()
+        {
+            return console.log("My name is " + this.name + " and I am " + this.age);
+        },
+        myHobbies: function() {
+            return console.log("Some of my hobbies include " + this.hobbies)}
+    }
+    me.learn();
+    me.myHobbies();
+}
+
+meExpanded();
 
 /************************************************************* */
-// Problem 6: Create an object literal that represents any object in the real world that you like.  Try to come up with a realistic set of properties and methods that would accurately describe that object.  In at least one of your methods, use the this keyword to refer back to one of the properties you defined on this object literal.  
+// Problem 7: Create an object literal that represents any object in the real world that you like.  Try to come up with a realistic set of properties and methods that would accurately describe that object.  In at least one of your methods, use the this keyword to refer back to one of the properties you defined on this object literal.  
 
 //your code...
 
+function phoneExpanded()
+{
+    var phone =
+    {
+        type: "iPhone", 
+        model: "X", 
+        year: "2017", 
+        software: "iOS 11",
+        storage: "64 GB",
+        features: "water reistance up to 30 feet",
+        learn: function()
+        {
+            return console.log("I have an " + this.type + this.model);
+        },
+        myFeatures: function()
+        {
+            return console.log("One feature is that it is " + this.features)
+        }
+    }
+    phone.learn();
+    phone.myFeatures();
+}
 
+phoneExpanded();
 
 /************************************************************* */
-//Problem 7: create a function that outputs your 3 favorite data types, with a message explaining why you like that data type.
+//Problem 8: create a function that outputs your 3 favorite data types, with a message explaining why you like that data type.
 
 //your code...
 
+function Data()
+{
 
+    console.log("Booleans are my first favorite because they super simple, like if you just wanted to know if an expression is true.");
+    console.log("Numbers are my second favorite because I've always had fun playing around with them.");
+    console.log("Strings are my third favorite because like booleans they are simple, but also can be used to store or manipulate text.");
+}
+
+Data();
 
 /************************************************************* */
 //Bonus assignments:

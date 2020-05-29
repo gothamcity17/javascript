82bfa7d021f63df4cc35f59332fcc6a593aaf0a5
Gowtham Katta <gowthamkatta@gmail.com>
2020-05-29 03:48:06 -0500

    Updated exercise 3

--- exercise3.js
+++ exercise3.js
@@ -5,64 +5,81 @@
 // Problem 1:
 // Write a JavaScript function that determines whether a string is empty. If the string is empty, the output should be true, otherwise it should return false. 
 
-
-
 //your code...
 
+var MyString = ""
 
+function StringStatus ()
+{
+    if (MyString === "") console.log("True");
+    else console.log("False");
+}
 
-
+StringStatus();
 
 /************************************************************* */
 
 // Problem 2:
 // Declare a variable that holds a string (e.g., ‘Learning JavaScript is fun!’). Replace a word so that the output uses the new word instead (e.g., ‘Learning JavaScript is cool!). 
 
-
-
 //your code...
 
+function Replace()
+{
+    var FirstString = "Learning JavaScript is fun!";
+    FirstString = FirstString.split(' ');
+    console.log(FirstString);
+    FirstString[3] = "cool!";
+    FirstString = FirstString.join(' ');
+    console.log(FirstString);
+}
 
-
+Replace();
 
 /************************************************************* */
 // Problem 3:
 // Write a function that declares an array of numbers from one to five and returns another array with the squared value of each number from the first array. Use an arrow function and one of the built-in array methods. 
 
-
-
 //your code...
 
+function Declares()
+{
+    var FirstArray = [1, 2, 3, 4, 5]
+    var SecondArray = FirstArray.map(x => Math.pow(x,2));
+    console.log(SecondArray);
+}
 
-
-
-
+Declares()
 
 /************************************************************* */
 // Problem 4:
 // Now declare another array of numbers that has the following numbers: 1, 3, 5, 7, 9, 1, 3, 5. Write a JavaScript program that returns an array of the numbers that are greater than 3. Use an arrow function and one of the built-in array methods.
 
-
-
 //your code...
 
+function Declares()
+{
+    var FirstArray = [1, 3, 5, 7, 9, 1, 3, 5];
+    var SecondArray = FirstArray.filter(x => {if(x>3) return x})
+    console.log(SecondArray);
+}
 
-
-
-
+Declares()
 
 /************************************************************* */
 // Problem 5:
 // Now declare another array of numbers that has any 5 numbers. Write a JavaScript program that returns the sum of those numbers. Use an arrow function and one of the built-in array methods. 
 
-
-
 //your code...
 
+function Declares()
+{
+    var FirstArray = [5, 10, 15, 20, 25]
+    var SecondArray = FirstArray.reduce((x, y) => x + y, 0);
+    console.log(SecondArray)
+}
 
-
-
-
+Declares()
 
 /************************************************************* */
 // Problem 6:
@@ -70,14 +87,35 @@
 
 // Write a function that takes in a DNA string as a parameter and returns an array with the complementary strand. For example, a string of "GCTA" would return an array of ["C", "G", "A", "T"].  
 
-
-
 //your code...
 
+const DNA_String = "GCTAAAAAA"
 
+function ComplementaryStrand()
+{
+    var DNA_Array = DNA_String.split('')
+    console.log(DNA_Array);
+    var DNA_Array_Reverse = DNA_Array.map (x => 
+    {
+        switch(x)
+        {
+            case "C":
+                return "G";
+            case "T":
+                return "A";
+            case "G":
+                return "C";
+            case "A":
+                return "T";
+            default:
+                return;
+                break;
+        }
+    })
+    console.log(DNA_Array_Reverse);
+}
 
-
-
+ComplementaryStrand()
 
 /************************************************************* */
 // Problem 7:
@@ -104,6 +142,11 @@ function sortNums(numbers,desc=false) {
 
 const mapObj = new Map();
 mapObj.set({company : "TEKsystems"},"object");
+mapObj.set(23, "integer");
+mapObj.set(26.5, "float");
+mapObj.set(true, "boolean");
+mapObj.set("An example of a sentence", "string");
+console.log(mapObj)
 
 
 console.log(mapObj.has({company : "TEKsystems"}));  
@@ -111,24 +154,40 @@ console.log(mapObj.has({company : "TEKsystems"}));
 
 //your code...
 
+console.log("The above console.log() statement is false as there is not a conrete reference to rely upon.")
+
 //loop through the mapObj and create a new array of only the data types, leaving out the example keys of the mapObj.  Use array methods to do this.  Example output : ['string',number','boolean',array','object']
 
+var New_Array = [];
+mapObj.forEach(x => {New_Array.push(x)})
+console.log(New_Array)
 
 /************************************************************* */
-//Problem 11:
+//Problem 9:
 
 let ones = [1,11,111,1111,11111,111111,1111111,11111111,111111111,1111111111];
 //reverse the array, without modifying the ones array.
 
+let onesReversed = ones.slice().reverse();
+
+console.log(onesReversed)
 
 /************************************************************* */
-//Problem 12:
+//Problem 10:
 //create a function called performer(cb) that takes in a callback function and runs that callback function.  It should return the output of the callback function.
 
-function performer(cb) {
-    //code goes here
+function greeting()
+{
+    var name = prompt('Please enter your name.');
+    alert('How are you doing,' + " " +name + "?");
 }
-
+  
+function performer(cb)
+{
+    cb();
+}
+  
+performer(greeting);
 
 /************************************************************* */
 //Bonus assignment:

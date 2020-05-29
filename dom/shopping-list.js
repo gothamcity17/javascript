fa416b2f3782baed7feb620640acf0521bff1b33
Gowtham Katta <gowthamkatta@gmail.com>
2020-05-29 11:07:46 -0500

    Updated dom html file Added dom css file Add dom js file

---
+++ dom/shopping-list.js
@@ -0,0 +1,15 @@
+var ul = document.getElementById("ul");
+var input = document.getElementById("input");
+var button = document.getElementById("button");
+
+function myButton() {
+    document.getElementById("button").innerHTML = "YOU CLICKED ME";
+}
+
+function getInput() {
+    document.getElementById("input").value;
+}  
+
+var li = document.getElementById("li");
+var span = document.getElementById("span");
+var buttonTwo = document.getElementById("buttonTwo");

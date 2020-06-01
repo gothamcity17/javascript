5d4f168c149e91aba85544355db7440018c08355
Gowtham Katta <gowthamkatta@gmail.com>
2020-05-31 22:05:29 -0500

    Completed Part 1

--- dom/shopping-list.js
+++ dom/shopping-list.js
@@ -1,15 +1,37 @@
-var ul = document.getElementById("ul");
-var input = document.getElementById("input");
-var button = document.getElementById("button");
+var link = document.createElement('link');
 
-function myButton() {
-    document.getElementById("button").innerHTML = "YOU CLICKED ME";
+var shop_ul = document.getElementsByTagName("ul");
+var shop_input = document.getElementById("item");
+var shop_button = document.getElementsByTagName("button");
+
+
+shop_button[0].addEventListener("click", buttonAdd);
+
+function buttonAdd() 
+{
+    var input_value = shop_input.value;
+    shop_input.value = "";
+
+
+    var shop_li = document.createElement("li");
+    var shop_span = document.createElement("span");
+    var second_button = document.createElement("button");
+
+
+    shop_li.append(shop_span);
+    shop_li.append(second_button);
+
+
+    shop_span.innerHTML = input_value;
+    second_button.innerHTML = "delete"
+
+    shop_ul[0].append(shop_li);
+
+    second_button.addEventListener("click", buttonDelete);
+
+    function buttonDelete()
+    {
+        child.parentNode.removeChild(shop_li);
+    }
+      shop_input.focus();
 }
-
-function getInput() {
-    document.getElementById("input").value;
-}  
-
-var li = document.getElementById("li");
-var span = document.getElementById("span");
-var buttonTwo = document.getElementById("buttonTwo");
